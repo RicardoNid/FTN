4590ec1f1a3878e710505d57899abb10c7398db2
Tianrui Li <38556098+RicardoNid@users.noreply.github.com>
2021-04-29 13:23:23 +0800

    重整了接收机iterating的形式,使得FDE的所属更加清晰,循环真正成为一个循环,三部分与图对应,在此基础上可以绘制时空图

--- Iterating.m
+++ Iterating.m
@@ -1,37 +1,34 @@
-function decoded = Iterating(decoded, i, FDE)
-    global On
+function diffQAMSymbols = Iterating(FDE)
     global IsPreamble
     global PowerOn
-    global RmsAlloc
     global Iteration
 
     %% 关于FDE的命名我又做了仔细考虑,之前内外功率分配同时去除是对外部的QAM解映射产生了影响,而不是功率分配累积
     % Iterating内是局部作用域,FDE命名没有side effcet,因此改回FDE
     % 更进一步说,matlab没有按引用传递,只有按值传递,所以所有side effct只能通过返回值产生
 
-    %% 关于此部分代码的理解请参见图NO-DMT DSP
-    QAMSymbols = Bits2QAM(decoded); % 旁路1,QAMSymbols
+    diffQAMSymbols = FDE;
 
-    PowerOn = 1; % 加功率分配
-    QAMSymbols = PowerOnOff(QAMSymbols);
+    for iter = 1:Iteration
 
-    IsPreamble = 0;
-    OFDMSymbols = IFFT(QAMSymbols);
+        PowerOn = 0; % 去功率分配
+        diffQAMSymbols = PowerOnOff(diffQAMSymbols);
 
-    recovered = FFT(OFDMSymbols); % 旁路2,recovered
+        recvBits = QAM2Bits(diffQAMSymbols); % QAM解映射 -> 解交织 -> 维特比译码
 
-    %% 旁路汇集部分
-    ICI = recovered - QAMSymbols;
-    dataQAMSymbols = FDE - ICI;
+        %% 关于此部分代码的理解请参见图NO-DMT DSP
+        QAMSymbols = Bits2QAM(recvBits); % 旁路1,QAMSymbols
 
-    PowerOn = 0; % 去功率分配
-    dataQAMSymbols = PowerOnOff(dataQAMSymbols);
+        PowerOn = 1; % 加功率分配
+        QAMSymbols = PowerOnOff(QAMSymbols);
 
-    decoded = QAM2Bits(dataQAMSymbols);
+        IsPreamble = 0;
+        OFDMSymbols = IFFT(QAMSymbols);
 
-    if On == 0 && i == Iteration
-        % ?? 此处可能也是不必要的
-        dataQAMSymbols = dataQAMSymbols * RmsAlloc(4);
+        recvQAMSymbols = FFT(OFDMSymbols); % 旁路2,recovered
 
-        Alloc(dataQAMSymbols);
+        %% 旁路汇集部分
+        ICI = recvQAMSymbols - QAMSymbols;
+        diffQAMSymbols = FDE - ICI;
+
     end
--- OFDMFrameReceiver.m
+++ OFDMFrameReceiver.m
@@ -2,16 +2,14 @@ function [decoded] = OFDMFrameReceiver(recvOFDMFrame)
     global On
     global IsPreamble
     global PowerOn
-    global Iteration
     global CPLength
     global DataCarrierPositions
     global PreambleNumber
     global SToPcol
     global FFTSize
+    global RmsAlloc
 
-    %% 信道估计和修正
-    % hardware: 不同于功率分配,信道估计必然是动态的,不能做成半静态
-    % 信道估计和修正作用于驻留的FDE
+    %% 下面部分对应于图1
     recvPreambleOFDMSymbols = recvOFDMFrame(1:PreambleNumber * (FFTSize + CPLength)); % 将收到的子帧分为训练序列和信息序列
     recvMsgOFDMSymbols = recvOFDMFrame(PreambleNumber * (FFTSize + CPLength) + 1:end);
 
@@ -21,6 +19,7 @@ function [decoded] = OFDMFrameReceiver(recvOFDMFrame)
     IsPreamble = 0;
     FDE = FFT(recvMsgOFDMSymbols); % fft,FDE尺寸224*16
 
+    % 信道估计和均?
     H = ChannelEstimation(reambleQAMSymbols); % 信道估计,得到(训练序列所占据的)各个子载波上的修正系数,H尺寸255*1
 
     % 信道均?
@@ -28,30 +27,17 @@ function [decoded] = OFDMFrameReceiver(recvOFDMFrame)
         FDE(:, i) = FDE(:, i) ./ H(DataCarrierPositions - 1); % ?? 此处的子载波对齐可能有误
     end
 
-    % ORIGINAL!
-    % 到这一步,得到FDE
+    %% 下面部分对应于图2
+    diffQAMSymbols = Iterating(FDE);
 
-    %% 下面描述迭代通路
-    % 将FDE分为两支，一支卸载功率分配之后进入迭代通路，另一支直接驻留在迭代入口，而不是卸载功率分配后，又在迭代通路中加载功率分配
-    FDEforIterating = FDE; % 驻留在迭代入口
-
+    %% 下面部分对应于图3
     PowerOn = 0; % 去功率分配
-    FDE = PowerOnOff(FDE);
+    diffQAMSymbols = PowerOnOff(diffQAMSymbols);
 
-    decoded = QAM2Bits(FDE); % QAM解映射 -> 解交织 -> 维特比译码
+    decoded = QAM2Bits(diffQAMSymbols); % QAM解映射 -> 解交织 -> 维特比译码
 
-    % ?? FDEforIterating是否要进行归一化
-    for iter = 1:Iteration
-        decoded = Iterating(decoded, iter, FDEforIterating); % 在接收机中迭代译码,并且,在训练模式下进行比特分配
+    if On == 0
+        % ?? 此处可能也是不必要的
+        dataQAMSymbolsForAlloc = diffQAMSymbols * RmsAlloc(4);
+        Alloc(dataQAMSymbolsForAlloc);
     end
-
-    % ORIGINAL!!
-
-    % dataQAMSymbols = IteratingBetter(FDE);
-
-    % if On == 1 % 去功率分配
-    %     PowerOn = 0;
-    %     dataQAMSymbols = PowerOnOff(dataQAMSymbols);
-    % end
-
-    % decoded = QAM2Bits(dataQAMSymbols); % QAM解映射 -> 解交织 -> 维特比译码
